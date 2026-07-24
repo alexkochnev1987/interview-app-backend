54c376cec35f68160e37d499c448eaf29de02c8a
roryTheGnome <s29991@pjwstk.edu.pl>
2026-07-24 13:07:33 +0200

    fix - fix openapi error

--- openapi/generated/api.d.ts
+++ openapi/generated/api.d.ts
@@ -2493,14 +2493,6 @@ export interface operations {
                     "application/json": components["schemas"]["ApiErrorResponseDto"];
                 };
             };
-            403: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["ApiErrorResponseDto"];
-                };
-            };
             404: {
                 headers: {
                     [name: string]: unknown;
