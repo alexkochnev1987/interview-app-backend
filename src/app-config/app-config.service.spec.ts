8e2fb11dd151f2c9c06be25d3104266d6471d548
ihorkuhel-dev <ihor.kuhel@innowise.com>
2026-08-24 13:27:15 +0200

    fix: fix test

--- src/app-config/app-config.service.spec.ts
+++ src/app-config/app-config.service.spec.ts
@@ -252,7 +252,7 @@ describe('AppConfigService', () => {
         RECRUITER_ASSISTANT_ENABLED: true,
         ENABLE_AI_ASSISTANT: true,
         ENABLE_LIVE_TRANSCRIPT: true,
-        LIVE_TRANSCRIPT_DEFAULT_EXPANDED: true,
+        LIVE_TRANSCRIPT_DEFAULT_EXPANDED: false,
       });
       expect(publicVars.PRIVATE_SETTING).toBeUndefined();
       expect(publicVars.PUBLIC_SECRET_KEY).toBeUndefined();
